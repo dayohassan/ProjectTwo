579c25ec0acf62e5d7aed407b2add1769a057265
stephengallo1977 <stephengallo1977@gmail.com>
2019-08-14 14:30:03 -0400

    adding alot

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,12 +1,21 @@
 const db = require('../models')
 
-module.exports = function(app){
-// This handles the POST request while also using the promise function
-    app.post("/api/addlisting", function(req, res){
+module.exports = function (app) {
+    // This handles the POST request while also using the promise function
+    app.post("/api/addlisting", function (req, res) {
         db.House.create(req.body)
-            .then(function(dbData){
-                res.redirect("/")
+            .then(function (dbData) {
+                res.redirect("/listing")
             })
     })
+
+    app.get("/api/deleteListings", function (req, res) {
+        db.House.destroy({
+            truncate: true
+        }).then(function (data) {
+            res.redirect("/")
+        })
+    })
+
 
 }
